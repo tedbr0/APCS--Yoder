955c0c0f22785b4a0763ba4b1abdbdbdeca8f7f7
tedbr0 <75226501+tedbr0@users.noreply.github.com>
2023-01-09 20:32:20 -0500

    Add files via upload

--- T1ExamC/src/Moutain.java
+++ T1ExamC/src/Moutain.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Moutain 
 {
@@ -12,7 +13,12 @@ public class Moutain
 	}
 	public static boolean isDecreasing(int[] array, int start)
 	{
-		
+		for(int i = start; i <= array.length; i++)
+		{
+			if(array[i] < array[i + 1])
+				return false;
+		}
+		return true;
 	}
 	public static int getPeakIndex(int[] array)
 	{
@@ -30,5 +36,22 @@ public class Moutain
 		int peak = getPeakIndex(array);
 		return(peak!=-1 && isIncreasing(array, peak) == true && isDecreasing(array, peak) == true);
 	}
+	public static void main(String[] args) 
+	{
+		int[] m1 = new int[] {5 ,3, 2, 9, 0};  
+		int[] m2 = new int[] {5, 6, 9 ,8, 7};
+		int[] m5 = new int[]  {1, 2, 3, 4, 9};
+		String[] str = new String[] {"hi", "lol", "bla"};
+		
+		System.out.println("Increasing?: " + isIncreasing(m2, 0));
+		System.out.println("Decreasing?: " + isDecreasing(m2, 0));
+		System.out.println("Peak index: " +getPeakIndex(m2));
+		System.out.println("Mountain?: " + isMountain(m1));
+		System.out.println("Mountain?: " + isMountain(m2));
+//		System.out.println("P1A(Sum of entries): " + arraySum(m5)); //P1 a Works
+//		System.out.println("P1B(Sum of a row): " + Arrays.toString(rowSums(m1))); //P1 b works
+//		System.out.println("P1C(Diverse matrix): " + isDiverse(m1));
+//		System.out.println("P1C(Diverse matrix): " + isDiverse(m6)); //P1 C works
 
+	}
 }
